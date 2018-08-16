6afdb752e0085f1410c1b3ba68672cf595f0c320
Michal Niegrzybowski <micnie@jetshop.se>
2018-08-16 08:10:50 +0200

    remove anoying `this`

--- src/Validator/Option.cs
+++ src/Validator/Option.cs
@@ -4,32 +4,22 @@
 
     public struct Option<T>
     {
-        private readonly T some;
+        private readonly T _some;
+        public bool IsNone => !IsSome;
+        public readonly bool IsSome;
 
         private Option(T some)
         {
-            this.IsSome = some != null;
-            this.some = some;
+            IsSome = some != null;
+            _some = some;
         }
-
-        public bool IsNone => !IsSome;
-
-        public bool IsSome { get; }
 
         public static Option<T> Some(T value) => new Option<T>(value);
         public static Option<T> None() => new Option<T>();
 
-        public T Value
-        {
-            get
-            {
-                if (this.IsNone)
-                {
-                    throw new InvalidOperationException("Cannot access Some of None");
-                }
-
-                return this.some;
-            }
-        }
+        public T Value => 
+            IsNone
+                ? throw new InvalidOperationException("Cannot access Some of None")
+                : _some;
     }
 }
